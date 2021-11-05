418265a3f2ae59efdbf9ceccde6cb1bcb27f0da3
Зорихина Майя <90088355+GhostLorde@users.noreply.github.com>
2021-11-05 21:56:53 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,41 @@
-import socket
+import socket, threading
+CLIENT = 0  # количество всех клиентов, имеющих возможность одновременно подключаться
+lock = threading.Lock()  # создает замок для всех потоков
+
+def connect(conn: socket.socket, addr: tuple) -> None:
+
+    print(addr[0])
+    while True:
+        data = conn.recv(1024)
+        ans = data.decode()
+        print(ans)
+        if not data:
+            break
+
+        conn.send(ans.upper().encode())
+    conn.close()
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+try:
+    num_port = int(input("Введите номер порта "))
+    assert 1024 < num_port < 65535, "Ваше значение не удовлетворяет условию ввода. "
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+except (AssertionError, TypeError, ValueError) as e:
+    num_port = 9092
 
-print(msg)
+print("Точка подключения: ", num_port)
 
-conn.close()
+sock.bind(("", num_port))
+print('Успешно')
+
+CLIENT = int(input("Введите количество прослушиваемых одновременно портов "))
+
+sock.listen(CLIENT)
+
+if __name__ == "__main__":
+
+    while True:
+        conn, addr = sock.accept()
+        thread = threading.Thread(target=connect, args=[conn, addr])
+        thread.start()
